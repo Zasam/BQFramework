7830bc154b1dcd6d83f47b36a3aca088db1e2b1f
Nicklas Wagener <nicklasw@outlook.de>
2021-04-05 15:35:49 +0200

    Implemented file helper and async task helper

---
+++ BQFramework/IO/FileHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace BQFramework.IO
+{
+    public static class FileHelper
+    {
+        /// <summary>Read text from a file within the specified assembly</summary>
+        /// <param name="filename">The name of the file to read text from</param>
+        /// <param name="assembly">Use typeof(classInAssembly).GetTypeInfo().Assembly to determine the current assembly where the textfile is located</param>
+        /// <returns></returns>
+        public static string GetTextFromFile(string filename, Assembly assembly)
+        {
+            try
+            {
+                var stream = assembly.GetManifestResourceStream(filename);
+                var text = string.Empty;
+
+                if (stream != null)
+                {
+                    using var reader = new StreamReader(stream);
+                    text = reader.ReadToEnd();
+                }
+
+                return text;
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
